c83bbf3b39b8b6aff56c95df3e5d95c93df940d6
kamesh <kameshkabil9@gmail.com>
2024-01-03 21:46:38 +0530

    README.md File updated Successfully

--- src/main/java/com/example/dtoslearning/config/SpringLocationConfig.java
+++ src/main/java/com/example/dtoslearning/config/SpringLocationConfig.java
@@ -1,2 +1,19 @@
-package com.example.dtoslearning.config;public class SpringLocationConfig {
-}
+//package com.example.dtoslearning.config;
+//
+//
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SpringLocationConfig {
+//
+//    @Value("spring.datasource.url")
+//    private String dataBaseURL;
+//
+//    @PostConstruct
+//    public void printDataBaseURL(){
+//        System.out.println(dataBaseURL);
+//    }
+//}
